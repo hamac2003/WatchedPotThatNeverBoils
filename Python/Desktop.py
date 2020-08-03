edc6b8753b6caf01af1d133c391a9a28d630bf74
hamac2003 <41019535+hamac2003@users.noreply.github.com>
2020-08-03 08:51:50 -0400

    Add files via upload

--- Python/Desktop.py
+++ Python/Desktop.py
@@ -16,7 +16,7 @@ import time
 #Below are links to some of the example code and/or libraries that I integrated into my project.
 
 """
-[Facial Detection](https://www.pyimagesearch.com/2018/02/26/face-detection-with-opencv-and-deep-learning/)
+[Facial Detection (Neural Network)](https://www.pyimagesearch.com/2018/02/26/face-detection-with-opencv-and-deep-learning/)
 
 [Raspberry Pi / Desktop - Arduino Serial Communication](https://roboticsbackend.com/raspberry-pi-arduino-serial-communication/)
 
--- Python/RaspberryPi.py
+++ Python/RaspberryPi.py
@@ -16,7 +16,7 @@ from gpiozero import LED
 #Below are links to some of the example code and/or libraries that I integrated into my project.
 
 """
-[Facial Detection](https://www.pyimagesearch.com/2018/02/26/face-detection-with-opencv-and-deep-learning/)
+[Facial Detection (Dlib)](https://www.pyimagesearch.com/2018/04/02/faster-facial-landmark-detector-with-dlib/)
 
 [Raspberry Pi / Desktop - Arduino Serial Communication](https://roboticsbackend.com/raspberry-pi-arduino-serial-communication/)
 
